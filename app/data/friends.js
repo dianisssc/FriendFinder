e396801f7b147fd94b11bafdb8c5ca095df73123
Dianis Carbajal <dianiscarbajal@gmail.com>
2019-04-27 12:21:57 -0400

    added friends and modified routing to work accuartely

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+//create array of default friends
+
+var friends = 
+
+[{
+    name: "Aziz Ansari",
+    photo: "https://images.app.goo.gl/7ooxtJTCfhNBtgtw7",
+    scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
+  }, {
+    name: "Adam Scott",
+    photo: "https://images.app.goo.gl/ubVf8A8HbNuSHLEq6",
+    scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
+  }, {
+    name: "Amy Poehler",
+    photo: "https://images.app.goo.gl/JFoiGLeVLuTssmy1A",
+    scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
+  },{
+    name: "Aubrey Plaza",
+    photo: "https://images.app.goo.gl/fVaMDnmv6vJTwSdQ6",
+    scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
+  }, {
+    name: "Chris Pratt",
+    photo: "https://images.app.goo.gl/CTc3w1ZGcKCi9diz9",
+    scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
+  }, {
+    name: "Rashida Jones",
+    photo: "https://images.app.goo.gl/DVEBvSovbkGBh29M7",
+    scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
+  }, {
+    name: "Paul Schneider",
+    photo: "https://images.app.goo.gl/7toWZHMYQ9KbQpBG6",
+    scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
+  }];
+  
+   //export array
+  
+  module.exports = friends;
